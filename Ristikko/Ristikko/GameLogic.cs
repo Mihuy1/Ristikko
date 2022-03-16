eb763f763d13d1396738d23567a254f8d02d63b2
Mihuy1 <s2000954@edu.bc.fi>
2022-03-16 17:00:51 +0200

    Update GameLogic.cs
    Nyt voi voittaa diagonaalisesti myös.

--- Ristikko/Ristikko/GameLogic.cs
+++ Ristikko/Ristikko/GameLogic.cs
@@ -14,12 +14,12 @@ namespace Ristikko
         private const string O = "O";
         private string[,] Board = new string[3, 3];
 
-        public void SetNextPlayer () 
-        { 
-            if (CurrentPlayer == X) 
+        public void SetNextPlayer()
+        {
+            if (CurrentPlayer == X)
             {
                 CurrentPlayer = O;
-            } else 
+            } else
             {
                 CurrentPlayer = X;
             }
@@ -40,21 +40,36 @@ namespace Ristikko
             //}
 
             // we want to the colums
-            for (var i = 0; i < 3; i++)
-            {
-                if (!String.IsNullOrWhiteSpace(Board[0, i]))
-                {
-                    if (Board[0, i] == Board[1, i] && Board[0, i] == Board[2, i])
-                    {
-                        return true;
-                    }
-                }
-            }
+            //for (var i = 0; i < 3; i++)
+            //{
+            //    if (!String.IsNullOrWhiteSpace(Board[0, i]))
+            //    {
+            //        if (Board[0, i] == Board[1, i] && Board[0, i] == Board[2, i])
+            //        {
+            //            return true;
+            //        }
+            //    }
+            //}
 
             // Diagonal
 
+            if (!String.IsNullOrWhiteSpace(Board[1, 1]))
+            {
+                if (Board[0, 0] == Board[1, 1] && Board[1, 1] == Board[2, 2])
+                {
+                    return true;
+                }
+
+                if (Board[0, 2] == Board[1, 1] && Board[1, 1] == Board[2, 0])
+                {
+                    return true;
+                }
+            }
+        
+
             return false;
         }
+    
 
         internal void UpdateBoard(Position position, string value)
         {
